5bbfd025911740cb0ffa2000e83a5c7a66f63fdd
justinmeckmann1 <info@meckimac.com>
2026-01-21 11:58:11 -0500

    lec2

---
+++ Lec/lec02/Hello.java
@@ -0,0 +1,20 @@
+package Lec.lec02;
+
+/**
+* This is the first simple program in Core Java Chapter 3.
+*
+* @version 1.01 1997-03-22
+* @author Gary Cornell
+*/
+
+public class Hello {
+    /**
+    * The main method serves as the entry point for the Java application.
+    * It prints "Hello, Word!" to the console.
+    * Here, we are using the System.out object and calling its println method.
+    * @param args command-line arguments passed to the program
+    */
+    public static void main(String[] args) {
+        System.out.println("Hello, Word!");
+    }
+}
