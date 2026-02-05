9b423b04feef353486288ec2a3038f8522bcf0f7
justinmeckmann1 <info@meckimac.com>
2026-02-05 11:49:22 -0500

    fixed lab 3

--- lab/lab03/SavingsSimulator_v2.java
+++ lab/lab03/SavingsSimulator_v2.java
@@ -11,10 +11,10 @@ public class SavingsSimulator_v2 {
         System.out.println("Username: " + System.getProperty("user.name"));
         System.out.println("Time: " + java.time.LocalDateTime.now().toString());
         
-        int years = 0;              // initializing years, starting with 0
+        int years = 1;              // initializing years, starting with 1
         double payment = 500;       // this could also be an int. Used double to adress a case, where the payment might not be a whole number 
         double balance = 0;         // starting balance
-        double interestRate = 0.05; // 5% interest rate 
+        double interestRate = 5; // 5% interest rate 
         double goal = 10000;        // goal this could also be an int. Used double to adress a case, where the goal might not be a whole number 
 
         while (years <= 100 && balance < goal) { // including balance check in the condition for the while loop
@@ -30,5 +30,7 @@ public class SavingsSimulator_v2 {
         
             years++; 
         } 
+
+        System.out.println("Number of years to reach the goal: " + years);
     }
 }
